59c5ce897e64ae2b59eab1ca51f613da779d0dfd
MS <msz@>
2024-07-22 14:19:41 -0700

    Add templates and static restaurant

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def sayHello(request):
-    return HttpResponse('Hello World')
+def index(request):
+    return render(request, 'index.html', {})
