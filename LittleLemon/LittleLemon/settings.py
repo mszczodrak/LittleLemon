54193c3cde3795ab01750531e0d11514a201dcf0
MS <msz@>
2024-07-23 10:58:53 -0700

    mysql

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-#DATABASES = {  
-#    'default': {  
-#        'ENGINE': 'django.db.backends.mysql',  
-#        'NAME': 'my_database',  
-#        'USER': 'root',  
-#        'PASSWORD': 'your_password',  
-#        'HOST': '127.0.0.1',  
-#        'PORT': '3306',  
-#        'OPTIONS': {  
-#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#        }  
-#    }  
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 #}
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'my_database',  
+        'USER': 'root',  
+        'PASSWORD': 'your_password',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}
 
 
 # Password validation
